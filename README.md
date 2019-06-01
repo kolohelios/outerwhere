47bf7c1ef52ac447f1fc9709385eeddaeeccad72
Jon Edwards <jkedwards@me.com>
2019-06-01 16:21:50 -0700

    add TaskScheduler support
    lint c_cpp_properties.json
    add secrets info to README.md
    replace LED test code with code using TaskScheduler

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # outerwhere
 
 IoT demo
+
+## Secrets
+
+There should be a file in the root project folder called `arduino_secrets.h`. Within it should be the following:
+
+```C++
+#define SECRET_SSID "{your SSID here}"
+#define SECRET_PASSPHRASE "{your passphrase here}"
+
+```
--- outerwhere.ino
+++ outerwhere.ino
@@ -1,32 +1,72 @@
 #include "src/FadeLED/FadeLED.h"
 #include "src/WiFiConnectivity/WiFiConnectivity.h"
 #include "Arduino.h"
+#include "arduino_secrets.h"
+#include "TaskScheduler.h"
 
 FadeLED led(D5, D7, D6, false);
-WiFiConnectivity wifiConnectivity(<SSID>, <PASSPHRASE>>);
+WiFiConnectivity wifiConnectivity(SECRET_SSID, SECRET_PASSPHRASE);
+
+// Callback methods prototypes
+void t1Callback();
+void t2Callback();
+void t3Callback();
+
+// Tasks
+Task t4();
+Task t1(3000, TASK_FOREVER, &t1Callback);
+Task t2(6000, TASK_FOREVER, &t2Callback);
+Task t3(9000, TASK_FOREVER, &t3Callback);
+
+Scheduler runner;
+
+void t1Callback()
+{
+  Serial.print("blue");
+  led.setColor(0, 0, 5);
+  led.turnOn();
+}
+
+void t2Callback()
+{
+  Serial.print("red");
+  led.setColor(10, 0, 0);
+  led.turnOn();
+}
+
+void t3Callback()
+{
+  Serial.print("green");
+  led.setColor(0, 10, 0);
+  led.turnOn();
+}
 
 void setup()
 {
-  // put your setup code here, to run once:
   Serial.begin(115200);
+  Serial.println("Scheduler TEST");
+
+  runner.init();
+  Serial.println("Initialized scheduler");
+
+  runner.addTask(t1);
+  Serial.println("added t1");
+
+  runner.addTask(t2);
+  Serial.println("added t2");
+
+  runner.addTask(t3);
+  Serial.println("added t3");
+
+  t1.enable();
+  Serial.println("Enabled t1");
+  t2.enable();
+  Serial.println("Enabled t2");
+  t3.enable();
+  Serial.println("Enabled t3");
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  led.setColor(0, 0, 5);
-  led.turnOn();
-  delay(5000);
-  led.turnOff();
-  delay(5000);
-  led.setColor(5, 0, 0);
-  led.turnOn();
-  delay(5000);
-  led.turnOff();
-  delay(5000);
-  led.setColor(0, 10, 0);
-  led.turnOn();
-  delay(5000);
-  led.turnOff();
-  delay(5000);
+  runner.execute();
 }
